64cfab8a82639dfa4056fb3ced69a5d43f666243
School <justin.battrick@mohawkcollege.ca>
2024-11-23 00:01:49 -0500

    sets up the player icon.

--- Group5-a4-SpaceInvaders/player.cs
+++ Group5-a4-SpaceInvaders/player.cs
@@ -4,9 +4,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Group5_a4_SpaceInvaders
+namespace Game10003
 {
-    internal class player
+    public class player
     {
+
+        public player()
+        {
+            drawPlayer();
+            playerMovement();
+        }
+
+        private void drawPlayer()
+        {
+            Draw.LineSize = 3;
+            Draw.LineColor = Color.Black;
+            Draw.FillColor = Color.Blue;
+
+            Draw.Rectangle(400, 450,100,100);
+            Draw.Rectangle(375, 400, 50, 50);
+            Draw.Rectangle(425, 400, 50, 50);
+
+        }
+
+        private void playerMovement()
+        {
+
+        }
+        
     }
 }
