10d5bd1c0978ff370a6bab51d3b1557a1f184fb9
School <justin.battrick@mohawkcollege.ca>
2024-11-23 00:09:35 -0500

    sets up player motion logical ***UNTESTED***

--- Group5-a4-SpaceInvaders/player.cs
+++ Group5-a4-SpaceInvaders/player.cs
@@ -8,6 +8,9 @@ namespace Game10003
 {
     public class player
     {
+        // Initalize position
+        int mainRectangleX = 400;
+
 
         public player()
         {
@@ -21,15 +24,25 @@ namespace Game10003
             Draw.LineColor = Color.Black;
             Draw.FillColor = Color.Blue;
 
-            Draw.Rectangle(400, 450,100,100);
-            Draw.Rectangle(375, 400, 50, 50);
-            Draw.Rectangle(425, 400, 50, 50);
+            Draw.Rectangle(mainRectangleX, 450,100,100);
+            Draw.Rectangle(mainRectangleX - 25, 400, 50, 50);
+            Draw.Rectangle(mainRectangleX + 25, 400, 50, 50);
 
         }
 
         private void playerMovement()
         {
+            if(Input.IsKeyboardKeyPressed(KeyboardInput.A)) {
 
+                // Updates the x position dynamically to redraw the player 25 points to the left of the current position
+                mainRectangleX -= 25;
+
+            } else if (Input.IsKeyboardKeyPressed(KeyboardInput.D)) {
+
+                // Updates the x position dynamically to redraw the player 25 points to the right of the current position
+                mainRectangleX += 25;
+
+            }
         }
         
     }
